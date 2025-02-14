34a6187bd65b9a7caa19ddbfbef226cd36888744
MWJOB <okkilltogo@gmail.com>
2025-02-14 18:27:43 +0900

    init.

---
+++ src/main/java/org/example/devinterviewscraper/DevInterviewScraperApplication.java
@@ -0,0 +1,13 @@
+package org.example.devinterviewscraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevInterviewScraperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevInterviewScraperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/devinterviewscraper/DevInterviewScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.devinterviewscraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevInterviewScraperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
